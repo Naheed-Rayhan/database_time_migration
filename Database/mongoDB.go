5ae70e572663dc2be322e6bb2a4a004c96b02002
Naheed-Rayhan <naheed.rayhan@shikho.com>
2025-01-28 00:19:49 +0600

    decopuled mongodb connection

---
+++ Database/mongoDB.go
@@ -0,0 +1,41 @@
+package Database
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"log"
+)
+
+func ConnectToMongoDB(uri string) (*mongo.Client, error) {
+	// Connect to MongoDB
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, err
+	}
+	// ping the connection
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("Connected to MongoDB")
+
+	return client, nil
+}
+
+func GetCollection(client *mongo.Client, dbName, collectionName string) *mongo.Collection {
+	// Get a handle for your collection
+	collection := client.Database(dbName).Collection(collectionName)
+	return collection
+}
+
+func DisconnectMongoDB(client *mongo.Client) error {
+	// Disconnect from MongoDB
+	err := client.Disconnect(context.TODO())
+	if err != nil {
+		return err
+	}
+	log.Printf("Disconnected from MongoDB")
+
+	return nil
+}
